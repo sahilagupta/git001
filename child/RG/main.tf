d8e7b7ea21d5ded63a8cb193b56c5654ed107091
Sahil Gupta <sahil.a.gupta145@gmail.com>
2026-01-30 00:04:01 +0530

    new

--- child/RG/main.tf
+++ child/RG/main.tf
@@ -27,3 +27,9 @@ resource "azurerm_resource_group" "rg-rohit24" {
   location = "east us"
     tags     = {}  # Azure best practice: avoid empty tag maps
 }
+
+resource "azurerm_resource_group" "rg-rohit4" {
+  name     = "rohit4"
+  location = "east us"
+    tags     = {}  # Azure best practice: avoid empty tag maps
+}
